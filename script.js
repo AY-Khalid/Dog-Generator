4f96cdf028078eeec70124ff50e6f2094b4673b8
AYKhalid <aniduyakubu@yahoo.com>
2022-06-06 19:13:05 +0100

    sixth commit

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function generateDog() {
     let image = document.createElement("img");
     let div = document.getElementById("flex-dog-gen");
     image.src = "http://thedogapi.com/api/images/get?format=src&type=gif&size=small"; 
+    image.style.width = "75%";
     div.appendChild(image);  
 }
